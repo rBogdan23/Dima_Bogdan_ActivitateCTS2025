d18bdc2809f35aa7499f87ad9439ecc6daac4dac
bogdan <bogdan.k983@gmail.com>
2025-04-02 13:11:04 +0300

    persoana fizica

---
+++ Banca/src/p2/classes/ClientPersoanaFizica.java
@@ -0,0 +1,21 @@
+package p2.classes;
+
+public class ClientPersoanaFizica implements Client {
+    private String nume;
+    private String cnp;
+
+    public ClientPersoanaFizica(String nume, String cnp) {
+        this.nume = nume;
+        this.cnp = cnp;
+    }
+
+    @Override
+    public void descriere() {
+        System.out.println("Client Persoana Fizica: " + nume + ", CNP: " + cnp);
+    }
+
+    @Override
+    public String getNume() {
+        return nume;
+    }
+}
