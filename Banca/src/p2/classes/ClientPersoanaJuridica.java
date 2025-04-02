59d4395c4b589509d7ee25c39fed1b8dc0e1b2e9
bogdan <bogdan.k983@gmail.com>
2025-04-02 13:11:13 +0300

    persoana juridica

---
+++ Banca/src/p2/classes/ClientPersoanaJuridica.java
@@ -0,0 +1,21 @@
+package p2.classes;
+
+public class ClientPersoanaJuridica implements Client {
+    private String nume;
+    private String cui;
+
+    public ClientPersoanaJuridica(String nume, String cui) {
+        this.nume = nume;
+        this.cui = cui;
+    }
+
+    @Override
+    public void descriere() {
+        System.out.println("Client Persoana Juridica: " + nume + ", CUI: " + cui);
+    }
+
+    @Override
+    public String getNume() {
+        return nume;
+    }
+}
