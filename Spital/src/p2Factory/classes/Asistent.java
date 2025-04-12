b0afbd9e669ad4c2e4c551d85d499a8557b5478c
bogdan <bogdan.k983@gmail.com>
2025-04-12 14:41:18 +0300

    Problema2 Factory creearea claselor Asistent si Brancardier

---
+++ Spital/src/p2Factory/classes/Asistent.java
@@ -0,0 +1,8 @@
+package p2Factory.classes;
+
+public class Asistent  implements PersonalSpital {
+    @Override
+    public void descriere() {
+        System.out.println("Sunt un asistent.");
+    }
+}
---
+++ Spital/src/p2Factory/classes/Brancardier.java
@@ -0,0 +1,8 @@
+package p2Factory.classes;
+
+public class Brancardier implements PersonalSpital {
+    @Override
+    public void descriere() {
+        System.out.println("Sunt un brancardier.");
+    }
+}
