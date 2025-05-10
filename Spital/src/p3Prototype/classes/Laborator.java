348228df928f81f98634e1d7f179779918d947ec
bogdan <bogdan.k983@gmail.com>
2025-05-10 11:04:27 +0300

    Clase prototype spital

---
+++ Spital/src/p3Prototype/classes/Laborator.java
@@ -0,0 +1,20 @@
+package p3Prototype.classes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Laborator {
+    private Map<String, Reteta> retete = new HashMap<>();
+
+    public void adaugaReteta(String nume, Reteta reteta) {
+        retete.put(nume, reteta);
+    }
+
+    public Reteta obtineReteta(String nume) throws CloneNotSupportedException {
+        Reteta reteta = retete.get(nume);
+        if (reteta != null) {
+            return reteta.clone();
+        }
+        throw new IllegalArgumentException("Reteta necunoscuta.");
+    }
+}
